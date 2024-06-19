74c3c2887ac3588b1d1aed40fe1229cd56c31451
brahimelkaceh <brahimelkaceh@gmail.com>
2024-06-19 22:26:27 +0100

    commit

--- src/sections/_home/color-mapping.tsx
+++ src/sections/_home/color-mapping.tsx
@@ -30,6 +30,7 @@ const datasource: any = data as any;
 registerLicense(
   'Ngo9BigBOggjHTQxAR8/V1NAaF1cVGhIfEx1RHxQdld5ZFRHallYTnNWUj0eQnxTdEFjX35dcXJRRGFVV0ZyWw=='
 );
+/* eslint-disable no-unused-vars */
 
 const ColorMap = () => {
   const mapInstance = useRef<MapsComponent>(null);
@@ -90,8 +91,6 @@ const ColorMap = () => {
               border={{ color: '#fff', width: 2 }}
               dataSource={datasource.locals}
               tooltipSettings={{
-                template:
-                  '<div id="markertooltiptemplate" style="width: 160px;opacity: 90%;background: rgba(53, 63, 76, 0.90);box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.40);padding:10px;border: 1px #abb9c6;border-radius: 4px;"><div style="font-size:13px;color:#ffffff;font-weight: 500;"><center>${latitude}</center></div><hr style="margin-top: 2px;margin-bottom:5px;border:0.5px solid #DDDDDD"><div><span style="font-size:13px;color:#cccccc">Population  : </span><span style="font-size:13px;color:#ffffff;font-weight: 500;">${latitude}</span></div>',
                 visible: true,
                 valuePath: 'latitude',
               }}
@@ -102,4 +101,6 @@ const ColorMap = () => {
     </MapsComponent>
   );
 };
+/* eslint-enable */
+
 export default ColorMap;
