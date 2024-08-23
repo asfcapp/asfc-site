a75dc1c9cf9978ad94bbeba5076030867580acad
youssef-mous <106970977+youssef-mous@users.noreply.github.com>
2024-08-23 17:39:27 +0100

    update queries and add a new plugin next-sanity

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -15,6 +15,8 @@ export async function fetchCampaignsWithInfractions() {
       }),
       seo->
     }`;
+  // Todo we must other fields of infractions,
+  return client.fetch(query);
 }
 export async function fetchBlog() {
   const query = ` *[_type == "blog"] {
@@ -26,6 +28,7 @@ export async function fetchBlog() {
   //  category, Retrieves the category of the blog
   // isDisplayedOnHome Indicates if the blog should be displayed on the homepage
   // order(publishedAt desc)  Sorts results by publication date in descending order
+  return client.fetch(query);
 }
 export async function fetchPartners() {
   const query = `*[_type == "partner"] {
@@ -37,6 +40,7 @@ export async function fetchPartners() {
   // description Retrieves the description of the partner
   // partnerType Retrieves the type of the partner
   // order(name) Sorts results by name in ascending order
+  return client.fetch(query);
 }
 export async function fetchRoj() {
   const query = `*[_type == "roj"]{
@@ -53,6 +57,7 @@ export async function fetchRoj() {
   // body Retrieves the body of the Roj
   // imageRoj Retrieves the image of the Roj
   // seo Retrieves the SEO information of the Roj
+  return client.fetch(query);
 }
 export async function fetchTags() {
   const query = ` *[_type == "tag"] {
@@ -64,6 +69,7 @@ export async function fetchTags() {
   // slug Retrieves the slug of the tag
   // description Retrieves the description of the tag
   // order(title) Sorts results by title in ascending order
+  return client.fetch(query);
 }
 export async function fetchAboutUs() {
   const query = `*[_type == "aboutUs"]{
@@ -74,6 +80,7 @@ export async function fetchAboutUs() {
   // title Retrieves the title of the About Us page
   // slug Retrieves the slug of the About Us page
   // body Retrieves the body of the About Us page
+  return client.fetch(query);
 }
 export async function fetchCommuniques() {
   const query = `*[_type =="communiques"]{
@@ -86,290 +93,5 @@ export async function fetchCommuniques() {
   // publishedAt Retrieves the publication date of the communique
   // illustrations Retrieves the illustrations image with some metadata  of the communique
   // isDisplayedOnHome Indicates if the communique should be displayed on the homepage
-}
-export async function fetchHomeData() {
-  const query = `*[_type == "home"][0]{
-    sectionTitle,
-    about,
-    aboutdescription,
-    rows[] {
-      title,
-      content
-    },
-    seo{
-      metaTitle,
-      metaDescription,
-      keywords
-    }
-  }`;
-  return client.fetch(query);
-}
-export async function fetchRojData() {
-  const query = `*[_type == "roj"]{
-    _id,
-    title,
-    slug,
-    subtitle,
-    image,
-    sectionTitle,
-    sectionSubTitle,
-    sectionDescription,
-    partenaire[]->{
-      _id,
-      photo,
-      name,
-      review
-    },
-    seo {
-      metaTitle,
-      metaDescription,
-      keywords,
-    }
-  }[0]`;
-  return client.fetch(query);
-}
-
-export async function fetchCampagnesData() {
-  const query = `*[_type == "campagne"]{
-    _id,
-    title,
-    image,
-    description,
-    _updatedAt,
-    _createdAt,
-    partenaire[]->{
-      _id,
-      photo,
-      name,
-      review
-    },
-    delit[]->{
-      _id,
-      _createdAt,
-      title,
-      "file": pdf.asset->url,
-      photo,
-      description,
-      articleDeLoi{
-        title,
-        description,
-        files[] {
-          asset->{
-            _id,
-            url
-          },
-          originalFilename,
-          size
-        }
-      },
-      explication,
-      dataMaroc,
-      observation,
-      publication
-    },
-    seo {
-      metaTitle,
-      metaDescription,
-      keywords,
-    }
-  }`;
-  return client.fetch(query);
-}
-
-export async function fetchDelitDataById(delitId: string) {
-  const query = `*[_type == "delit" && _id == $delitId]{
-    _id,
-    _createdAt,
-    title,
-    photo,
-    description,
-    articleDeLoi,
-    explication,
-    dataMaroc,
-    observation,
-    publication{
-      title,
-      policy,
-      etudes,
-      presse[]->{
-        _id,
-        title,
-        slug,
-        author->{
-          _id,
-          name,
-          image{
-            asset->{
-              _id,
-              url
-            }
-          },
-          bio,
-          instagram,
-          linkedin,
-          facebook,
-          website,
-          dateJoined
-        },
-        mainImage{
-          asset->{
-            _id,
-            url
-          }
-        },
-        categories[]->{
-          _id,
-          title
-        },
-        publishedAt,
-        body,
-        tag[]->{
-          _id,
-          title
-        }
-      }
-    }
-  }`;
-
-  const params = { delitId };
-  return client.fetch(query, params);
-}
-export async function fetchPartenaires() {
-  const query = `*[_type == "partenaire"]{
-  photo,
-  name,
-  review
-  }`;
-  return client.fetch(query);
-}
-
-export async function fetchPosts() {
-  const query = `*[_type == "post"]{
-    _id,
-    title,
-    slug,
-    author->{
-      _id,
-      name,
-      image,
-      bio,
-      instgram,
-      linkedin,
-      facebook,
-      website,
-      dateJoined
-    },
-    mainImage,
-    categories[]->{
-      _id,
-      title
-    },
-    publishedAt,
-    body,
-    tag
-  }`;
-  return client.fetch(query);
-}
-
-export async function fetchPostById(id: string) {
-  const query = `*[_type == "post" && _id == $id][0]{
-    _id,
-    title,
-    slug,
-     author->{
-      _id,
-      name,
-      image,
-      bio,
-      instagram,
-      linkedin,
-      facebook,
-      website,
-      dateJoined
-    },
-    mainImage,
-    categories[]->{
-      _id,
-      title
-    },
-    publishedAt,
-    body,
-    tag[]->{
-      _id,
-    title,
-    }
-  }`;
-
-  try {
-    const post = await client.fetch(query, { id });
-    return post;
-  } catch (error) {
-    console.error('Error fetching post by id:', error);
-    return null;
-  }
-}
-
-export async function fetchRecentPosts(limit: number = 4) {
-  const query = `*[_type == "post"] | order(publishedAt desc)[0...$limit]{
-    _id,
-    title,
-    slug,
-    author->{
-      _id,
-      name,
-      image,
-      bio,
-      instagram,
-      linkedin,
-      facebook,
-      website,
-      dateJoined
-    },
-    mainImage,
-    categories[]->{
-      _id,
-      title
-    },
-    publishedAt,
-    body,
-    tag
-  }`;
-  return client.fetch(query, { limit });
-}
-// lib/queries.ts
-
-export async function fetchHomePosts() {
-  const query = `*[_type == "post" && displayOnHomePage == true] {
-    _id,
-    title,
-    slug,
-    author->{
-      _id,
-      name,
-      image,
-      bio,
-      instagram,
-      linkedin,
-      facebook,
-      website,
-      dateJoined
-    },
-    mainImage,
-    categories[]->{
-      _id,
-      title
-    },
-    publishedAt,
-    body,
-    tag
-  }`;
-  return client.fetch(query);
-}
-
-export async function fetchCategories() {
-  const query = `*[_type == "category"]{
-  _id,
-  title,
-  }`;
   return client.fetch(query);
 }
