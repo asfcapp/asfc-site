ada0d12a5293cc53e7c0fe7f39c93ea077785776
youssef-mous <106970977+youssef-mous@users.noreply.github.com>
2024-09-20 15:01:41 +0100

    add the block content to infraction detail view

--- src/sections/asfc-sections/_campaigns/_Infraction-details/Infraction-view.tsx
+++ src/sections/asfc-sections/_campaigns/_Infraction-details/Infraction-view.tsx
@@ -1,16 +1,19 @@
 'use client';
 
+import { Typography } from '@mui/material';
 import Grid from '@mui/material/Unstable_Grid2';
 import Container from '@mui/material/Container';
 
 import { paths } from 'src/routes/paths';
 
 import urlFor from 'src/lib/sanity';
-// import { _caseStudies } from 'src/_mock';
+import { _caseStudies } from 'src/_mock';
 
 import Image from 'src/components/image';
 import RichText from 'src/components/rich-text/rich-text';
 import CustomBreadcrumbs from 'src/components/custom-breadcrumbs';
+
+import InfractionPublication from 'src/sections/asfc-sections/_campaigns/_Infraction-details/Infraction-publication';
 
 // ----------------------------------------------------------------------
 
@@ -20,10 +23,10 @@ type Props = {
 };
 export default function InfractionView({ infraction }: Props) {
   const contentFields = [
-    infraction.lawArticle,
-    infraction.explanation,
-    infraction.moroccanData,
-    infraction.observation,
+    { key: 'Article de loi', value: infraction.lawArticle },
+    { key: 'Explication', value: infraction.explanation },
+    { key: 'Données marocaines', value: infraction.moroccanData },
+    { key: 'Observation', value: infraction.observation },
   ];
 
   return (
@@ -56,12 +59,26 @@ export default function InfractionView({ infraction }: Props) {
 
       <Grid container spacing={{ xs: 5, md: 8 }} direction={{ md: 'row-reverse' }}>
         <Grid xs={12} md={4}>
-          {/* <InfractionPublication publication={_caseStudies[0]} /> */}
+          <InfractionPublication publication={_caseStudies[0]} />
         </Grid>
 
         <Grid xs={12} md={8}>
+          <Typography variant="h4" sx={{ mb: 5 }}>
+            Brève description
+          </Typography>
+          <Typography variant="body1" sx={{ mb: 5 }}>
+            {infraction.briefDescription}
+          </Typography>
           {contentFields.map((content: any, index) => (
-            <RichText key={index} content={content} />
+            <>
+              {content.value && (
+                <Typography variant="h4" sx={{ mb: 2, mt: 5 }}>
+                  {content.key}
+                </Typography>
+              )}
+              {/* show the title of the field only if the field exist  */}
+              <RichText key={index} content={content.value} />
+            </>
           ))}
           {/* <InfractionData images={_mockCaseStudy.galleryImgs} /> */}
         </Grid>
